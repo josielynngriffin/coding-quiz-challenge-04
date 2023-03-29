53f781434d6c379682bf6d5bb02a9e0afba73042
josielynngriffin <jelynngriffin@gmail.com>
2023-03-29 19:14:16 -0400

    Add psuedocode

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//
+//start button > timer starts (visual in right corner)
+// start button -- code that shows first question
+//timer made with setInterval?
+
+// present question, generate content to represent answer / maybe score?
+//questions in array? iterate through questions
+// upon answer, give next question
+//if incorrect, subtract time from clock as penalty
+//if there are still questions, else statement
+
+//when all questions are answered or the timer runs out,
+
+
+// the game is over and initials can be logged to save high score
+//save to local storage and print to page?
+//button to return to start
