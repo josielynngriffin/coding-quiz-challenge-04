8f449a6ecd5a4a4ecb3970c3c634b0af137ab1c5
josielynngriffin <jelynngriffin@gmail.com>
2023-04-04 13:51:08 -0400

    Add question cycle function

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,11 @@ let questionContainer = document.getElementById('question-container');
 let introContainer = intro.children;
 console.log('introContainer: ' + introContainer);
 let timerContainer = document.getElementById('timer-container');
-
+let answerButton = document.getElementById('answer-btn');
 //introContainer.setAttribute = ('class', 'hidden');
-
+let questionContent = document.getElementById('question-content');
+let answersContainer= document.getElementById('answers-container');
+//questionContent.textContent = "test";
 //my questions
 let quizQuestions = [
     {
@@ -25,7 +27,7 @@ let quizQuestions = [
     },
     {
         question:"Which operator compares both value and type of data?",
-        answers: ["==", "", "===", ""],
+        answers: ["==", "!=", "===", "="],
         correctAnswer: "==="
     },
     {
@@ -40,8 +42,8 @@ let quizQuestions = [
     },
     {
         question: "How do you write an array?",
-        answers: ["let numbers=['1', '2']", "", "", ""],
-        correctAnswer: "",
+        answers: ["let numbers=['1', '2']", "fhdfghf", "hdghg", "ghfhg"],
+        correctAnswer: "let numbers=['1', '2']",
     }
 ]
 
@@ -55,6 +57,7 @@ function hideStartScreen() {
          introContainer[i].classList.add('hidden');
 }
 }
+//timer function
 let secondsLeft=60;
 let timerContent = document.createElement('p');
 timerContent.classList.add('timer--align')
@@ -70,14 +73,67 @@ function setTime() {
         }
     }, 1000);
 }
+
+//function to start game
 function startGame() {
     hideStartScreen()
     questionContainer.style.display = 'flex';
     //questionContainer.classList.remove('hidden');
     setTime();
+    questionCycle();
+    //timer to iterate through questions object/array (questionCycle)
 }
 //Add event listener to start button, hides the intro
 startButton.addEventListener("click", startGame);
+
+//question varables
+let currentQuestionI = 0;
+let finalQuestion = quizQuestions.length;
+let score=0;
+
+//function to present questions
+function questionCycle() {
+    if (currentQuestionI === finalQuestion) {
+        //end function for final score
+        return;
+    } else if (secondsLeft === 0) {
+    //end function for final score
+    clearInterval(timerInterval);
+    } else {
+    //function to iterate through question array
+    answersContainer.innerHTML= "";
+    let currentQuestion = quizQuestions[currentQuestionI];
+    questionContent.textContent = currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        //answerButton.textContent = answer;
+        let button = document.createElement('button');
+        button.classList.add('answer-btn');
+        button.textContent = answer;
+        answersContainer.appendChild(button)
+        
+
+        button.addEventListener('click', function() {
+            if (answer == currentQuestion.correctAnswer) {
+                secondsLeft;
+                score++;
+                console.log("correct choice");
+                console.log(score);
+            } else {
+                secondsLeft -= 10;
+                score--;
+                console.log("incorrect choice");
+                console.log(score);
+            }
+            currentQuestionI++;
+            questionCycle();
+            let totalScore = score;
+            console.log("total score = " + totalScore);
+        })
+    })
+
+}}
+//console.log("total score = " + totalScore);
 //start button > timer starts (visual in right corner)
 // start button -- code that shows first question
 //timer made with setInterval?
